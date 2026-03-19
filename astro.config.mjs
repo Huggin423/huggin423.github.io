84093c229bfebc491cd5ef78fe176edeed0d932a
Mao Xiaoyi <maoxiaoyi423@163.com>
2026-03-19 16:37:43 +0800

    chore: switch to user-site base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://huggin423.github.io',
-  base: '/Blog',
+  base: '/',
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
