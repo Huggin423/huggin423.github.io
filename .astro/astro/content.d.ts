6b3ed037fce02a40e56352cc7294ec917ea6ab8b
Mao Xiaoyi <maoxiaoyi423@163.com>
2026-03-22 14:09:44 +0800

    更新评价功能

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -155,6 +155,13 @@ declare module 'astro:content' {
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".md"] };
+"arrival.md": {
+	id: "arrival.md";
+  slug: "arrival";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
 "lottreditor.md": {
 	id: "lottreditor.md";
   slug: "lottreditor";
